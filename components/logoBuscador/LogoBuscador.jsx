d926ee44cb634ff842d62ec653bc661ff270fa14
Hernando Alvarado <luis.alvarado@vtex.com.br>
2021-09-24 13:51:45 -0500

    cambio buscador avances

--- components/logoBuscador/LogoBuscador.jsx
+++ components/logoBuscador/LogoBuscador.jsx
@@ -10,10 +10,12 @@ import styles from "./logoBuscador.module.scss"
 
 function LogoBuscador({ partner }) {
   const router = useRouter()
+  const [textSearch, setTextSearch] = useState("")
+  const [results, setResults] = useState([])
   const [showSearchBox, setShowSearchBox] = useState(false)
   const PUBLICATIONS_QUERY = gql`
-  query Publications($slug: String){
-    publications(slug: $slug, status: true){
+  query Publications($slug: String, $title: String){
+    publications(slug: $slug, status: true, title: $title){
       category
       description
       image_2
@@ -27,10 +29,16 @@ function LogoBuscador({ partner }) {
     }
   }`
 
-  const [getPublications, { data: products }] = useLazyQuery(PUBLICATIONS_QUERY)
+  const [getPublications, { data: products }] = useLazyQuery(PUBLICATIONS_QUERY, {
+    variables: {
+      title: textSearch
+    },
+    fetchPolicy: "no-cache", onCompleted: ({ publications }) => {
+      setResults(publications)
+    }
+  })
 
   useEffect(() => {
-    getPublications()
     if (window.screen.width > 420) setShowSearchBox(true)
   }, [])
 
@@ -38,6 +46,14 @@ function LogoBuscador({ partner }) {
     const slug = products.publications.find((x) => x.title == values).slug;
     router.push("/publicacion/" + slug);
   }
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(async () => {
+      await getPublications()
+    }, 3000)
+
+    return () => clearTimeout(delayDebounceFn)
+  }, [textSearch])
 
   return (<div id={styles.LogoBuscador} className={partner ? "partner" : ""}>
     <ul>
@@ -57,15 +73,18 @@ function LogoBuscador({ partner }) {
       </div>
       {
         <div className={styles.content_buscador} style={{ display: showSearchBox ? "block" : "none" }}>
-          <Autocomplete
-            freeSolo
-            className="input-buscador"
-            id={styles["free-solo-2-demo"]}
-            disableClearable
-            onChange={onTagsChange}
-            options={products?.publications ? products.publications.map((option) => option.title) : []}
-            renderInput={(params, ind) => <TextField inputProps={{ min: 0, style: { textAlign: "center" }, type: "search" }} {...params} key={ind} className={styles.buscador} label={<span><FontAwesomeIcon icon={faSearch} /> &nbsp;nintendo switch, ps5, controles de xbox</span>} />}
-          />
+          <TextField onChange={(e) => setTextSearch(e.target.value)} fullWidth label="nintendo switch, ps5, controles de xbox" variant="outlined" />
+          <div className={styles.results}>
+            <div className={styles.rows}>
+              {
+                results && results.map(item => {
+                  return <Link href={item.slug ? "/publicacion/[id]" : "javascript:void(0)"} as={item.slug ? `/publicacion/${item.slug}` : "javascript:void(0)"}>
+                    <a>{item.title}</a>
+                  </Link>
+                })
+              }
+            </div>
+          </div>
         </div>
       }
     </ul>
