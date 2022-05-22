a02d39a1b168a34dcad30122ce058c105965f585
Hernando Alvarado <hernando.halvarado@mercadolibre.com.co>
2022-05-22 14:05:58 -0500

    testing checkout

--- components/checkout/Checkout.jsx
+++ components/checkout/Checkout.jsx
@@ -1,12 +1,14 @@
 import React from "react"
+import Button from "../button/Button"
 
-export default class Checkout extends React.Component {
-	openchekout = e => {
+const Checkout = (props) => {
+
+	const openchekout = e => {
 		e.preventDefault();
 
 		var handler = window.ePayco.checkout.configure({
 			key: '15e4f81c38f66bb570bc33a5a97a5f0b',
-			test: false
+			test: true
 		});
 
 		let data = {
@@ -18,13 +20,13 @@ export default class Checkout extends React.Component {
 			tax_base: '100000',
 			tax: '19000',
 			country: "co",
-			lang: "en",
+			lang: "es",
 			external: "false",
 			extra1: "extra1",
 			extra2: "extra2",
 			extra3: "extra3",
-			confirmation: "http://confirmacion.php",
-			response: "http://payco.co",
+			confirmation: "https://confirmacion.php",
+			response: "https://payco.co",
 			name_billing: 'john doe',
 			address_billing: "Carrera 19 numero 14 91",
 			type_doc_billing: "cc",
@@ -34,15 +36,10 @@ export default class Checkout extends React.Component {
 		}
 		handler.open(data);
 	}
-	render() {
-		return (
-			<React.Fragment>
-				<input
-					type="submit"
-					placeholder="pagar"
-					onClick={this.openchekout} />
-			</React.Fragment>
 
-		)
-	}
+	return <React.Fragment>
+		<Button onClick={openchekout} color="blue">Pagar</Button>
+	</React.Fragment>
 }
+
+export default Checkout
