cb5806e08790301d3fbf3802bcfa053fd3a8225a
Hernando Alvarado <hernando.halvarado@mercadolibre.com.co>
2023-08-30 16:43:01 -0500

    getChallenges added

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -3,12 +3,16 @@ import publicationsMock from './mocks/publications';
 import home from './mocks/home'
 import getArticles from './mocks/getArticles'
 import validateLogin from './mocks/validateLogin'
+import getChallenges from './mocks/getChallenges'
 
 export default function handler(req, res) {
   const operationName = req.headers?.['operation-name']
   switch (operationName) {
     case 'getArticles':
       res.status(200).json(getArticles);
+      break;
+    case 'getChallenges':
+      res.status(200).json(getChallenges);
       break;
     case 'validateLogin':
       res.status(200).json(validateLogin);
---
+++ pages/api/mocks/getChallenges.tsx
@@ -0,0 +1,34 @@
+const getChallenges = [
+    {
+        currentPoints: 1,
+        deadLine: '17 Sep',
+        description: '',
+        finished: true,
+        id: '42d',
+        prizeCoins: 30,
+        targetPoints: 1,
+        title: 'Buy a product',
+    },
+    {
+        currentPoints: 4,
+        deadLine: '17 Sep',
+        description: '',
+        finished: false,
+        id: '42z',
+        prizeCoins: 40,
+        targetPoints: 5,
+        title: 'Invite 5 friends',
+    },
+    {
+        currentPoints: 1,
+        deadLine: '17 Sep',
+        description: '',
+        finished: true,
+        id: '34k',
+        prizeCoins: 15,
+        targetPoints: 1,
+        title: 'Share on instagram or twitter',
+    }
+]
+
+export default getChallenges
