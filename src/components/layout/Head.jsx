f27c332a406bc115ebdf03b666b7d47d65afbdf2
Hernando Alvarado <hernando.halvarado@mercadolibre.com.co>
2024-02-18 22:52:45 -0500

    probando variables de entorno

--- src/components/layout/Head.jsx
+++ src/components/layout/Head.jsx
@@ -15,13 +15,11 @@ const Header = ({ description, image, title, url }) => {
       <meta name='og:site_name' content='Pikplay' />
       <meta
         name='viewport'
-        content='width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0'
-      />
+        content='width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0' />
       <meta name='theme-color' content='#476E95' />
       <meta
         name='google-site-verification'
-        content='4IqXj9YLrm5eo3s_c3cTKcAqBwUhCf8qgJgL2sLtJko'
-      />
+        content='4IqXj9YLrm5eo3s_c3cTKcAqBwUhCf8qgJgL2sLtJko' />
       <meta name='twitter:description' content={description} />
       <meta name='keywords' value='' />
       <meta name='country' content='COL' />
@@ -29,6 +27,7 @@ const Header = ({ description, image, title, url }) => {
       <meta name='copyright' content='pikplay.co' />
       <meta name='language' content='es-CO'></meta>
       <meta httpEquiv='ScreenOrientation' content='autoRotate:disabled' />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> {/* TODO Quitar esto una vez se pueda hacer el request al back con https */}
       {/* Global site tag (gtag.js) - Google Ads: 941382150 */}
       <link rel='alternate' href={url} hrefLang='es-CO' />
       <link rel='canonical' href={url} />
