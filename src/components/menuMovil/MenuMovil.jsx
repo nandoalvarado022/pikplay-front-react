74d5fb317527d94846601b8660a79a2cd280edfa
Hernando Alvarado <hernando.halvarado@mercadolibre.com.co>
2024-07-15 11:57:51 -0500

    cambios

--- src/components/menuMovil/MenuMovil.jsx
+++ src/components/menuMovil/MenuMovil.jsx
@@ -6,6 +6,11 @@ import Link from 'next/link'
 import PreviewUser from '../previewUser/PreviewUser'
 import useSystemStore from '../../hooks/useSystem'
 import { useIAStore } from '../ia/IAstore'
+import { faWarehouse } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseDamage } from '@fortawesome/free-solid-svg-icons'
+import { faCampground } from '@fortawesome/free-solid-svg-icons'
+import { faGamepad } from '@fortawesome/free-solid-svg-icons'
 // import { useSelector } from 'react-redux'
 
 const { motion } = require('framer-motion')
@@ -20,18 +25,10 @@ const MenuMovil = () => {
   return (
     <div className={styles.MenuMovil}>
       <ol className={styles.mainOption}>
-        <Link href='/publicacion/crear' as='/publicacion/crear'>
-          <motion.a
-            legacyBehavior
-            className={styles.vender}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.8 }}>
-            <img
-              style={{ height: '38px' }}
-              src='https://icons.veryicon.com/png/o/miscellaneous/very-thin-linear-icon/camera-310.png'
-              alt='Publicar articulo'
-            />
-            <div className='f-s-10'>VENDER</div>
+        <Link href='/categoria/[id]' as='/categoria/playstation'>
+          <motion.a legacyBehavior whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+            <img src='/images/icons/play.svg' alt='Playstation' />
+            {/* <div className='f-s-10'>XBOX</div> */}
           </motion.a>
         </Link>
       </ol>
@@ -39,7 +36,7 @@ const MenuMovil = () => {
         <Link href='/categoria/[id]' as='/categoria/nintendo-switch'>
           <motion.a legacyBehavior whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
             <img style={{ height: '28px' }} src='/images/icons/nintendo.svg' alt='Nintendo switch' />
-            <div className='f-s-10'>NINTENDO</div>
+            {/* <div className='f-s-10'>NINTENDO</div> */}
           </motion.a>
         </Link>
       </ol>
@@ -47,10 +44,18 @@ const MenuMovil = () => {
         <PreviewUser />
       </ol>
       <ol className={styles.mainOption}>
-        <Link href='/categoria/[id]' as='/categoria/playstation'>
-          <motion.a legacyBehavior whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
-            <img src='/images/icons/play.svg' alt='Playstation' />
-            <div className='f-s-10'>XBOX</div>
+        <Link href='/' as='/'>
+          <motion.a
+            legacyBehavior
+            className={styles.vender}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}>
+            <img
+              style={{ height: '38px' }}
+              src='/images/icons/retro-2.svg'
+              alt='Publicar articulo'
+            />
+            {/* <div className='f-s-10'>VENDER</div> */}
           </motion.a>
         </Link>
       </ol>
@@ -61,7 +66,7 @@ const MenuMovil = () => {
         whileTap={{ scale: 0.8 }}>
         <a>
           <img src='/images/ia/4.svg' />
-          <div className='f-s-10'>AYUDA</div>
+          {/* <div className='f-s-10'>AYUDA</div> */}
         </a>
       </motion.ol>
     </div>
