bb7af994a96ba275ea276bc12350f80794f36af0
Hernando Alvarado <hernando.halvarado@mercadolibre.com.co>
2024-07-15 23:30:33 -0500

    cambios

--- src/components/profileSummaryExperience/EditProfileSummary.jsx
+++ src/components/profileSummaryExperience/EditProfileSummary.jsx
@@ -7,6 +7,7 @@ import CloseButton from '../closeButton/CloseButton'
 const EditProfileSummary = (props) => {
 	const { isEditProfile, setIsEditProfile } = props
 	const { userLogged, setUserLogged } = useSystemStore()
+	const { backgroundColor, backgroundImage } = userLogged
 	const colors = [
 		"linear-gradient(45deg, #3095b359, #dc1919b5)",
 		"linear-gradient(45deg, #3095b359, #81127eb5)"
@@ -23,30 +24,37 @@ const EditProfileSummary = (props) => {
 	}
 
 	return <div className={`${isEditProfile ? styles.active : ''} ${styles.EditProfileSummary}`}>
-		<CloseButton onClick={() => setIsEditProfile(false)} />
+		<div className={styles.topLine} onClick={() => setIsEditProfile(false)}></div>
+		{/* <CloseButton onClick={() => setIsEditProfile(false)} /> */}
 		<div className={styles.borderColor}>
 			<label htmlFor="">Color del borde</label>
 			<div className={styles.items}>
 				{colors.map(item => {
-					return <div onClick={() => handleChange({ backgroundColor: item })} className={styles.item} style={{ background: item }}></div>
+					return <div
+						className={`${backgroundColor == item ? styles.selected : ''} ${styles.item}`}
+						onClick={() => handleChange({ backgroundColor: item })}
+						style={{ background: item }}></div>
 				})}
 			</div>
 		</div>
+		<hr />
 		<div className={styles.backgroundImage}>
-			<label>Fondo:</label>
+			<label>Fondo</label>
 			<div className={styles.items}>
 				{
 					backgrounds.map(item => {
-						return <div className={styles.item} onClick={() => handleChange({ backgroundImage: item })}>
+						return <div
+							className={`${backgroundImage == item ? styles.selected : ''} ${styles.item}`}
+							onClick={() => handleChange({ backgroundImage: item })}>
 							<img src={item} alt="" />
 						</div>
 					})
 				}
 			</div>
 		</div>
-		<div className={styles.actions}>
+		{/* <div className={styles.actions}>
 			<Button color="blue">Guardar</Button>
-		</div>
+		</div> */}
 	</div>
 }
 
