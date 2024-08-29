12825e8a11ceede9fcfbb972a87cfb0d915929d9
Hernando Alvarado <hernando.halvarado@mercadolibre.com.co>
2024-08-29 08:06:13 -0500

    firebaseConfig

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,18 +1,33 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from 'firebase/firestore'
 import firebase from 'firebase/app'
-import 'firebase/storage'
+import 'firebase/messaging'
 
-const config = {
-  apiKey: 'AIzaSyD8sWfr7pF2ASJDTOU5UEJ3Z0mVHaYvP4I',
-  authDomain: 'mi-club2ruedas.firebaseapp.com',
-  databaseURL: 'https://mi-club2ruedas.firebaseio.com',
-  projectId: 'mi-club2ruedas',
-  storageBucket: 'mi-club2ruedas.appspot.com',
+const firebaseConfig = {
+  apiKey: "AIzaSyD8sWfr7pF2ASJDTOU5UEJ3Z0mVHaYvP4I",
+  appId: "1:892995264915:web:007c3b611ddcd7ba968866",
+  authDomain: "mi-club2ruedas.firebaseapp.com",
+  databaseURL: "https://mi-club2ruedas.firebaseio.com",
+  messagingSenderId: "892995264915",
+  projectId: "mi-club2ruedas",
+  storageBucket: "mi-club2ruedas.appspot.com",
 }
 
-if (!firebase.apps.length && config.projectId != null) {
-  firebase.initializeApp(config)
-}
+// if (!firebase.apps.length && firebaseConfig.projectId != null) {
+//   firebase.initializeApp(firebaseConfig)
+// }
 
-const storage = firebase.storage()
+/*const messaging = firebase.messaging()
+messaging
+  .requestPermission()
+  .then(function () {
+    console.log('tienes permisos')
+  })
+  .catch(function () {
+    console.log('Ocurrio error')
+  })*/
 
-export { storage, firebase }
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db }
